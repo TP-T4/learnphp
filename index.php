37f49868a3a8aa7a4c220de668418d513965c960
Tauri Pärli <tauri.parli@techno.ee>
2026-09-03 10:02:34 +0300

    strings

--- index.php
+++ index.php
@@ -1,23 +1,9 @@
 <?php
-
-    $test = 10;
-    $test = 3.1;
-    $test = "Hello World";
-    $test = true;
-    $test = null;
-    $test = new stdClass();
-    $test->name = "John Doe";
-    $test = [1,null,"Hello",true];
-    var_dump($test);
-    //asaadsdasdadasdasdasd
-    /**
-     * This is a comment
-     */
-    $test = 10 + 10;
-    $test = 10 - 5;
-    $test = 10 * 5;
-    $test = 10 / 2;
-    $test = 10 % 3;
-    $test = 10 ** 2;
-    var_dump($test);
+    $test = 'yolo';
+    $test = 'yolo' . 'life';
+    $test = $test . 'thug';
+    $test = $test .= 'life';
+    $name = 'tauri';
+    $age = 18;
+    $test = $name . ' is ' . $age . ' years old';
 ?>
