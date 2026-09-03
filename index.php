35f4c21d86cac59b0a014d30e02f60c94c10bb1c
Tauri Pärli <tauri.parli@techno.ee>
2026-09-03 09:39:08 +0300

    Add hello world and types example

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+    $test = 10;
+    $test = 3.1;
+    $test = "Hello World";
+    $test = true;
+    $test = null;
+    $test = new stdClass();
+    $test->name = "John Doe";
+    $test = [1,null,"Hello",true];
+    var_dump($test);
+    //asaadsdasdadasdasdasd
+    /**
+     * This is a comment
+     */
+?>
