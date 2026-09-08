dbda82440e639ade89d8e99938fccd5a1c1d56bd
Tauri Pärli <tauri.parli@techno.ee>
2026-09-08 09:23:32 +0300

    Add array examples

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php
-$test = 10 > 5;
-$test = 10 < 5;
-$test = 10 == 10;
-$test = 10 <= 10;
-$test = 10 >= 10;
-$test = 10 == '10';
-$test = 10 === '10';
-$test = 10 != '10';
-$test = 10 !== '10';
-$test = 0 == false;
+$test = array(1, 2, 3, 4, 5);
+$test = [1, 2, 3, 4, 5];
+$test = [1, 'adsasas', true, [1, 2, 3, 4, 5]];
+$test = [
+    'name' => 'Tauri',
+    'age' => 18,
+    3,
+    true,
+    100 => 'lol',
+    'troll'
+];
+var_dump($test[3][2]);
 var_dump($test);
 ?>
