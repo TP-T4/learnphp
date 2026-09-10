8a41ff439f813418950cb90dde1f2de5ecf3e850
Tauri Pärli <tauri.parli@techno.ee>
2026-09-10 08:59:17 +0300

    Add loops example

--- index.php
+++ index.php
@@ -1,15 +1,31 @@
 <?php
-$test = array(1, 2, 3, 4, 5);
-$test = [1, 2, 3, 4, 5];
-$test = [1, 'adsasas', true, [1, 2, 3, 4, 5]];
-$test = [
-    'name' => 'Tauri',
-    'age' => 18,
-    3,
-    true,
-    100 => 'lol',
-    'troll'
-];
-var_dump($test[3][2]);
-var_dump($test);
-?>
+for ($i = 0; $i<10; $i++){
+    var_dump($i);
+}
+
+for ($i = 9; $i>0; $i--){
+    var_dump($i);
+}
+
+for($i = 2; $i<1_000_000; $i*=2){
+    var_dump($i);
+}
+
+$time = time();
+$i=0;
+while(time() < $time+1){
+    var_dump($i++);
+}
+
+while(false) {
+    var_dump('WHILE');
+}
+
+do {
+    var_dump('DO');
+} while(false);
+
+$fruits = ['Apple', 'Banana', 'Cherry'];
+foreach($fruits as $key=>$fruit){
+    var_dump($key,$fruit);
+}
