9269b0002aed2f1159b05675eaa7fb692bc80413
TP-T4 <Tauri.Parli@tptlive.ee>
2026-09-18 09:05:01 +0300

    box?

--- index.php
+++ index.php
@@ -1,62 +1,59 @@
 <?php
-$num = 10;
-if($num > 10) {
-    var_dump('Bigger');
-} 
 
-if($num > 10) {
-    var_dump('Bigger');
-} else {
-    var_dump('Smaller');
+class box {
+    public $width;
+    public $height;
+    public $depth;
+    public $isopen = false;
+    public $hasbeenopened = false;
+
+    public function __construct($width, $height, $depth) {
+        $this->width = $width;
+        $this->height = $height;
+        $this->depth = $depth;
+    }
+
+    public function volume() {
+        return $this->width * $this->height * $this->depth;
+    }
+
+    public function open() {
+        if (!$this->isopen) {
+            $this->isopen = true;
+            $this->hasbeenopened = true;
+        }
+    }
 }
 
-if($num > 10) {
-    var_dump('Bigger');
-} else if($num < 10) {
-    var_dump('Smaller');
-} else {
-    var_dump('Equal');
+box1 = new box();
+box1->width = 10;
+box1->height = 5;
+box1->depth = 2;
+box1->open();
+
+box2 = new box();
+box2->width = 3;
+box2->height = 4;
+box2->depth = 6;
+box2->$isopen = false;
+
+$num1 = 1;
+$num2 = $num1;
+$num1 = 2;
+
+$box1 = new box();
+$box1->width = 1;
+$box2 = clone $box1;
+$box1->width = 2;
+var_dump($box1, $box2); // Outputs: 2
+
+$numbers = [1, 2, 3, 4, 5];
+
+for($i=0; $i < count($numbers); $i++) {
+   $n = $numbers[$i];
+   $n+=1;
 }
 
-$day = (int) date('w');
-$day = intval(date('w'));
-var_dump($day);
-if($day === 0) {
-    var_dump('Sunday');
-} elseif($day === 1) {
-    var_dump('Monday');
-} elseif($day === 2) {
-    var_dump('Tuesday');
-} elseif($day === 3) {
-    var_dump('Wednesday');
-} elseif($day === 4) {
-    var_dump('Thursday');
-} elseif($day === 5 || $day === 6) {
-    var_dump('Partyday');
-}else {
-    var_dump('Weirdday');
-}
-
-switch($day) {
-    case 0:
-        var_dump('Sunday');
-        break;
-    case 1:
-        var_dump('Monday');
-        break;
-    case 2:
-        var_dump('Tuesday');
-        break;
-    case 3:
-        var_dump('Wednesday');
-        break;
-    case 4:
-        var_dump('Thursday');
-        break;
-    case 5:
-    case 6:
-        var_dump('Partyday');
-        break;
-    default:
-        var_dump('Weirdday');
+foreach($numbers as $n) {
+   $n+=1;
 }
