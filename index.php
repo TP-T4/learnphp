ce63cd7668926d2b17a1b609a5faa5ab03526797
TP-T4 <Tauri.Parli@tptlive.ee>
2026-09-24 09:25:20 +0300

    simple router

--- index.php
+++ index.php
@@ -1,49 +1,21 @@
 <?php
-$title = 'World';
-$posts = [
-  [
-    'title' => 'Some World title 1',
-    'date' => 'January 1, 2021',
-    'author' => 'Pets',
-    'body' => 'Some World content 1',
-  ],
-  [
-    'title' => 'Some World title 2',
-    'date' => 'January 3, 2021',
-    'author' => 'Manivald',
-    'body' => 'Some World content 2',
-  ],
-  [
-    'title' => 'Some World title 3',
-    'date' => 'January 5, 2021',
-    'author' => 'Jorss',
-    'body' => 'Some World content 3',
-  ],
-  [
-    'title' => 'Some World title 4',
-    'date' => 'January 7, 2021',
-    'author' => 'Heli Kopter',
-    'body' => 'Some World content 4',
-  ],
-];
+if (preg_match('/\.(?:png|jpg|jpeg|gif|css|js)$/', $_SERVER["REQUEST_URI"])) {
+    return false;    // serve the requested resource as-is.
+}
 
+function dump(...$values) {
+    echo '<pre>';
+    var_dump(...$values);
+    echo '</pre>';
+}
 
-?>
-
-
-<?php include __DIR__ . '/partials/header.php'; ?>
-
-  <main class="container"> 
-    <?php include __DIR__ . '/partials/hero.php'; ?>
-    <?php include __DIR__ . '/partials/featured.php'; ?>
-
-      <div class="row g-5">
-        <div class="col-md-8">
-          <?php include __DIR__ . '/partials/posts.php'; ?>
-        </div>
-        <div class="col-md-4">
-          <?php include __DIR__ . '/partials/sidebar.php'; ?>
-        </div>
-      </div>
-    </main>
-<?php include __DIR__ . '/partials/footer.php'; ?>
+switch($_SERVER['REQUEST_URI']){
+    case '/':
+        include __DIR__ . '/../views/index.php';
+        break;
+    case '/us':
+        include __DIR__ . '/../views/us.php';
+        break;
+    default:
+        echo '404';
+}
